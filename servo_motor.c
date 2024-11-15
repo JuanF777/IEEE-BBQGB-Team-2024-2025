4e2c9f3efaddeed52d3e9a41dc24dde70f4e3b17
Aiden Shepler <142184319+aouress@users.noreply.github.com>
2024-11-15 18:31:02 -0500

    Add files via upload
    servo test

---
+++ servo_motor.c
@@ -0,0 +1,35 @@
+#include <pigpio.h>
+
+int servoPin = 12; 
+
+int main() {
+    //necessary initialization check 
+    if (gpioInitialise() < 0)
+    {
+        //initialization failed
+        printf(stderr, "Failed to initialize pigpio\n");
+        return 1; 
+    }
+
+    //test 
+    gpioServo(servoPin, 1500);
+    sleep(1); 
+    return 0; 
+
+}
+
+
+/*gpioSetMode(servoPin, PI_OUTPUT);
+gpioPWM(servoPin, 0);
+
+int servo_motor(pin, position) 
+{
+     int duty = position*(7/100)+4;
+     gpioSETPWMrange(pin, duty);
+
+}
+
+while True
+{
+    int
+}*/
